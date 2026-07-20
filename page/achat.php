8d60c44c9268e649793d937d1b46ea8035bf9bc3
i4voo <i4voandrianina@gmail.com>
2026-07-20 14:19:15 +0300

    V2 part

--- page/achat.php
+++ page/achat.php
@@ -1,23 +1,9 @@
 <?php
 session_start();
 include('../inc/function.php');
-if (isset($_GET['id_membre'])) {
-    $id_membre = $_GET['id_membre'];
-} 
-$initiale = get_initiale_user($id_membre); 
-?>
-
-<nav>
-<ul>
-    <li><a href="accueil.php?id_membre=<?php echo $id_membre; ?>">Accueil</a></li>
-    <li><a href="vendre.php?id_membre=<?php echo $id_membre; ?>">Vendre un produit</a></li>
-    <li><a href="ventes.php?id_membre=<?php echo $id_membre; ?>">Mes ventes</a></li>
-    <li><a href="index.php">Déconnexion</a></li>
-</ul>
-<p>
-    <span><?php echo $initiale; ?></span>
-</p>
-</nav>
+if (isset($_SESSION['id_membre'])) {
+    $id_membre = $_SESSION['id_membre'];
+}
 
 
 if(isset($_GET['idProd']) && !empty($_GET['idProd'])) {
--- page/traitement_login.php
+++ page/traitement_login.php
@@ -1,14 +1,19 @@
 <?php
+session_start();
 include('../inc/function.php') ;
+
 if (isset($_POST['etu'], $_POST['nom'])) {
     $id = inscription($_POST['etu'], $_POST['nom']);
-    header('Location: accueil.php?id_membre=' . $id);
+    $_SESSION['id_membre'] = $id;
+    header('Location: accueil.php');
     exit();
 }
+
 if (isset($_POST['etu'])) {
     $membre = checklogin($_POST['etu']);
     if ($membre) {
-        header('Location: accueil.php?id_membre=' . $membre['id_membre']);
+        $_SESSION['id_membre'] = $membre['id_membre'];
+        header('Location: accueil.php');
         exit();
     }
     $etu_inconnu = $_POST['etu'];
@@ -29,7 +34,7 @@ if (isset($_POST['etu'])) {
     <h2>Numéro ETU inconnu ! Veuillez saisir votre nom pour vous inscrire :</h2>
 
     <form action="traitement_login.php" method="post">
-       <input type="hidden" name="etu" value="<?php echo $etu_inconnu; ?>">
+       <input type="hidden" name="etu" value="<?php echo htmlspecialchars($etu_inconnu, ENT_QUOTES, 'UTF-8'); ?>">
         
         <p>Votre Nom : <input type="text" name="nom" required placeholder="Ex: Jean Dupont"></p>
         <input type="submit" value="S'inscrire et continuer">

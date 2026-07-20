62767a3d233ab3cdee7fd44ee4155a68c63bd4ae
tanjona48 <tanjona48@gmail.com>
2026-07-20 10:24:38 +0300

    Intégration finale V1

--- inc/connect.php
+++ inc/connect.php
@@ -1,4 +1,10 @@
 <?php
-function db_connect(){
-    
-}
+    function db_connect(){
+        static $connect;
+        $connect=mysqli_connect('localhost','root','','VENTE_PRODUIT');
+        if(!$connect){
+            die("Erreur de connexion : ".mysqli_connect_error());
+        }
+        mysqli_set_charset($connect,'utf8mb4');
+        return $connect;
+    }
--- inc/function.php
+++ inc/function.php
@@ -1,14 +1,6 @@
 <?php
-<<<<<<< HEAD
-    function db_connect(){
-        static $connect;
-        $connect=mysqli_connect('localhost','root','','VENTE_PRODUIT');
-        if(!$connect){
-            die("Erreur de connexion : ".mysqli_connect_error());
-        }
-        mysqli_set_charset($connect,'utf8mb4');
-        return $connect;
-    }
+
+
 
 
     function login($ETU){
